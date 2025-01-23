e63b4d41d3e95ecacfc20146e8a8d834a706c10c
巧将 平野 <2230018@ecc.ac.jp>
2025-01-23 19:56:13 +0900

    タイマーの処理を修正。

--- Source/Graphics/Timer.cpp
+++ Source/Graphics/Timer.cpp
@@ -9,6 +9,9 @@ Timer::Timer(bool countdown, int timeM, int timeS)
 
 	timeMSprite = std::make_unique<Text>();
 	timeSSprite = std::make_unique<Text>();
+
+	//! タイムのセミコロン
+	timeColon = std::make_unique<Sprite>("Data/Font/Colon.png");
 }
 
 Timer::~Timer()
@@ -46,10 +49,23 @@ void Timer::Update(float elapsedTime)
 }
 
 // 描画処理
-void Timer::Render(ID3D11DeviceContext* dc, Graphics& graphics, DirectX::XMFLOAT2 position = { 30, 0 })
+void Timer::Render(ID3D11DeviceContext* dc, Graphics& graphics, DirectX::XMFLOAT2 position, DirectX::XMFLOAT4 color)
 {
 	//! 分の描画
-	timeMSprite->RenderThousand(dc, false, (int)timeM, false, false, true, position.x, position.y);
+	timeMSprite->RenderThousand(dc, false, (int)timeM, false, false, true, position.x, position.y, 8.0f, 8.0f, 0, 28.0f, color.x, color.y, color.z, color.w);
 	//! 秒の描画
-	timeSSprite->RenderThousand(dc, false, (int)timeS, false, false, true, position.x + 80, position.y);
+	timeSSprite->RenderThousand(dc, false, (int)timeS, false, false, true, position.x + 80, position.y, 8.0f, 8.0f, 0, 28.0f, color.x, color.y, color.z, color.w);
+
+	//! テクスチャの大きさ
+	float textureWidth  = static_cast<float>(timeColon->GetTextureWidth());
+	float textureHeight = static_cast<float>(timeColon->GetTextureHeight());
+
+	//! タイムのセミコロンの描画
+	timeColon->Render(dc,
+		position.x + 24, position.y,
+		40, 40,
+		0, 0,
+		textureWidth, textureHeight,
+		0,
+		color.x, color.y, color.z, color.w);
 }
--- Source/Graphics/Timer.h
+++ Source/Graphics/Timer.h
@@ -15,7 +15,13 @@ public:
 	void Update(float elapsedTime);
 
 	// 描画処理
-	void Render(ID3D11DeviceContext* dc, Graphics& graphics, DirectX::XMFLOAT2 position);
+	void Render(ID3D11DeviceContext* dc, Graphics& graphics, DirectX::XMFLOAT2 position = { 30, 0 }, DirectX::XMFLOAT4 color = { 1, 1, 1, 1});
+
+	// 分を取得
+	float GetTimeM() { return (int)timeM; }
+
+	// 秒を取得
+	float GetTimeS() { return (int)timeS; }
 
 private:
 	float timeM = 0; // 分
@@ -23,6 +29,7 @@ private:
 
 	std::unique_ptr<Text> timeMSprite;
 	std::unique_ptr<Text> timeSSprite;
+	std::unique_ptr<Sprite> timeColon; // タイムのセミコロン
 
 	bool countdown = false; // カウントダウンするか
 };

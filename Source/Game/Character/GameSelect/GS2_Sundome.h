1700c6c4bb1a2f600414845724ce9597be245078
2230018 <2230018@MSI>
2025-05-08 17:03:57 +0900

    ゲーム選択画面の処理を書いた

--- Source/Game/Character/GameSelect/GS2_Sundome.h
+++ Source/Game/Character/GameSelect/GS2_Sundome.h
@@ -1,1 +1,74 @@
 #pragma once
+
+#include "Graphics//Model.h"
+#include "GameSelect.h"
+#include "Graphics/Graphics.h"
+#include "Other/Mathf.h"
+
+// 寸止め
+class GS2_Sundome : public GameSelect
+{
+public:
+	GS2_Sundome();
+	~GS2_Sundome() override;
+
+	// 更新処理
+	void Update(float elapsedTime) override;
+
+	// 描画処理
+	void Render(ID3D11DeviceContext* dc, Shader* shader) override;
+
+	// HPなどの描画
+	void SpriteRender(ID3D11DeviceContext* dc, Graphics& graphics) override;
+
+	// デバッグプリミティブ描画
+	void DrawDebugPrimitive() override;
+
+	// デバッグGUI
+	void DrawDebugGUI() override;
+
+	// 縄張り設定
+	void SetTerritory(const DirectX::XMFLOAT3& origin, float range);
+
+private:
+	// ターゲット位置をランダム設定
+	void SetRandomTargetPosition();
+
+	// 目標地点へ移動
+	void MoveToTarget(float elapsedTime, float speedRate);
+
+	// 待機ステートへ遷移
+	void TransitionWaitState();
+
+	// 待機ステート更新処理
+	void UpdateWaitState(float elapsedTime);
+
+	// プレイヤーとの接触処理
+	void CollisionVsPlayer();
+
+private:
+	// ステート
+	enum class State
+	{
+		Wait,
+	};
+
+	// アニメーション
+	enum Animation
+	{
+		Anim_Wait,
+	};
+
+private:
+	std::unique_ptr<Model> model;
+
+	State state = State::Wait;
+	DirectX::XMFLOAT3 targetPosition = { 0,0,0 };
+	DirectX::XMFLOAT3 territoryOrigin = { 0,0,0 };
+	float territoryRange = 10.0f;
+	float moveSpeed = 2.0f;
+	float turnSpeed = DirectX::XMConvertToRadians(360);
+	float stateTimer = 0.0f;
+	float searchRange = 5.0f;
+	float attackRange = 1.5f;
+};
---
+++ Source/Game/Character/GameSelect/GS4_OssanTataki.cpp
@@ -0,0 +1,194 @@
+#include "GS4_OssanTataki.h"
+#include <imgui.h>
+#include "Graphics/Graphics.h"
+#include "Other/Mathf.h"
+#include "Game/Character/Player/Player0_OniGokko.h"
+#include "Other/Collision.h"
+#include "Game/Camera/CameraController.h"
+#include "Game/Scene/SceneGameSelect.h"
+
+// コンストラクタ
+GS4_OssanTataki::GS4_OssanTataki()
+{
+	model = std::make_unique<Model>("Data/Model/GameSelect/Onigokko.mdl");
+
+	// モデルが大きいのでスケーリング
+	scale.x = scale.y = scale.z = 0.05f;
+
+	angle.y = DirectX::XMConvertToRadians(180);
+
+	collisionType = CollisionType::Box;
+	// 幅、高さ設定
+	width = 22.55f;
+	height = 7.0f;
+	depth = 2.5f;
+
+	collisionOffset.y = 0.0f;
+
+	gravity = 0.0f;
+}
+
+// デストラクタ
+GS4_OssanTataki::~GS4_OssanTataki()
+{
+	//delete model;
+}
+
+// 更新処理
+void GS4_OssanTataki::Update(float elapsedTime)
+{
+	// ステート毎の更新処理
+	switch (state)
+	{
+	case State::Wait:
+		UpdateWaitState(elapsedTime);
+		break;
+	}
+
+	// キャラクターの状態更新処理
+	UpdateGameObjectBaseState(elapsedTime);
+
+	// プレイヤーとの当たり判定
+	CollisionVsPlayer();
+
+	// モデルアニメーション更新
+	model->UpdateAnimation(elapsedTime);
+
+	// モデル行列更新
+	model->UpdateTransform(transform);
+}
+
+// 描画処理
+void GS4_OssanTataki::Render(ID3D11DeviceContext* dc, Shader* shader)
+{
+	shader->Draw(dc, model.get(), materialColor, opacity, emissiveColor, emissiveStrength);
+}
+
+// HPなどの描画
+void GS4_OssanTataki::SpriteRender(ID3D11DeviceContext* dc, Graphics& graphics)
+{
+}
+
+// デバッグプリミティブ描画
+void GS4_OssanTataki::DrawDebugPrimitive()
+{
+	// 基底クラスのデバッグプリミティブ描画
+	GameSelect::DrawDebugPrimitive();
+
+	DebugRenderer* debugRender = Graphics::Instance().GetDebugRenderer();
+}
+
+// デバッグGUI
+void GS4_OssanTataki::DrawDebugGUI()
+{
+	if (ImGui::TreeNode("GS0_Onigokko"))
+	{
+		ImGui::InputFloat3("Velocity", &velocity.x);
+
+		// トランスフォーム
+		if (ImGui::CollapsingHeader("Transform", ImGuiTreeNodeFlags_DefaultOpen))
+		{
+			// 位置
+			ImGui::DragFloat3("Position", &position.x, 0.01f);
+			// 回転
+			DirectX::XMFLOAT3 a;
+			a.x = DirectX::XMConvertToDegrees(angle.x);
+			a.y = DirectX::XMConvertToDegrees(angle.y);
+			a.z = DirectX::XMConvertToDegrees(angle.z);
+			ImGui::DragFloat3("Angle", &a.x, 0.01f);
+			angle.x = DirectX::XMConvertToRadians(a.x);
+			angle.y = DirectX::XMConvertToRadians(a.y);
+			angle.z = DirectX::XMConvertToRadians(a.z);
+			// スケール
+			ImGui::DragFloat3("Scale", &scale.x, 0.01f);
+		}
+
+		if (ImGui::CollapsingHeader("Collision", ImGuiTreeNodeFlags_DefaultOpen))
+		{
+			ImGui::DragFloat("Width", &width, 0.01f);
+			ImGui::DragFloat("Height", &height, 0.01f);
+			ImGui::DragFloat("Depth", &depth, 0.01f);
+			ImGui::DragFloat3("CollisionOffset", &collisionOffset.x, 0.01f);
+		}
+
+		ImGui::TreePop();
+	}
+}
+
+// 縄張り設定
+void GS4_OssanTataki::SetTerritory(const DirectX::XMFLOAT3& origin, float range)
+{
+	territoryOrigin = origin;
+	territoryRange = range;
+}
+
+// ターゲット位置をランダム設定
+void GS4_OssanTataki::SetRandomTargetPosition()
+{
+	// 縄張り範囲内でランダムな位置を生成
+	float randomX = Mathf::RandomRange(territoryOrigin.x - territoryRange, territoryOrigin.x + territoryRange);
+	float randomZ = Mathf::RandomRange(territoryOrigin.z - territoryRange, territoryOrigin.z + territoryRange);
+
+	// Y座標はスライムの高さに設定するか、固定の地面の高さに設定する
+	float randomY = territoryOrigin.y; // または固定値、例: 0.0f
+
+	// ターゲット位置に設定
+	targetPosition.x = randomX;
+	targetPosition.y = randomY;
+	targetPosition.z = randomZ;
+}
+
+// 目標地点へ移動
+void GS4_OssanTataki::MoveToTarget(float elapsedTime, float speedRate)
+{
+	// ターゲット方向への進行ベクトルを算出
+	float vx = targetPosition.x - position.x;
+	float vz = targetPosition.z - position.z;
+	float dist = sqrtf(vx * vx + vz * vz);
+	vx /= dist;
+	vz /= dist;
+
+	// 移動処理
+	Move3D(vx, vz, moveSpeed * speedRate);
+	Turn3DNotCancel(elapsedTime, vx, vz, turnSpeed * speedRate);
+}
+
+// 待機ステートへ遷移
+void GS4_OssanTataki::TransitionWaitState()
+{
+	state = State::Wait;
+
+	// タイマーをランダム設定
+	stateTimer = Mathf::RandomRange(3.0f, 5.0f);
+
+	// 待機アニメーション再生
+	model->PlayAnimation(Anim_Wait, true);
+}
+
+// 待機ステート更新処理
+void GS4_OssanTataki::UpdateWaitState(float elapsedTime)
+{
+}
+
+// プレイヤーとの接触処理
+void GS4_OssanTataki::CollisionVsPlayer()
+{
+	Player0_Onigokko& player = Player0_Onigokko::Instance();
+
+	DirectX::XMFLOAT3 outPosition;
+	if (Collision::IntersectBoxVsCylinder(
+		collisionPos,
+		width,
+		height,
+		depth,
+		player.GetPosition(),
+		player.GetRadius(),
+		player.GetHeight(),
+		outPosition))
+	{
+		//! シーンを切り替える
+		SceneGameSelect::gameSelect = SceneGameSelect::GameSelect::OssanTataki;
+		SceneGameSelect::sceneChange = true;
+		player.SetPosition(outPosition);
+	}
+}

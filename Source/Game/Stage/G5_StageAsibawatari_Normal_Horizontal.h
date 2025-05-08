6b85f024f73aeb76ee59393e0c1a732a0a12b6ed
2230018 <2230018@MSI>
2025-05-08 09:45:12 +0900

    足場渡りのステージを追加した

---
+++ Source/Game/Stage/G5_StageAsibawatari_Normal_Horizontal.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Graphics/Model.h"
+#include "Stage.h"
+
+// ステージ
+class G5_StageAsibawatari_Normal_Horizontal : public Stage
+{
+public:
+	G5_StageAsibawatari_Normal_Horizontal();
+	~G5_StageAsibawatari_Normal_Horizontal();
+
+	// 更新処理
+	void Update(float elapsedTime) override;
+
+	// 描画処理
+	void Render(ID3D11DeviceContext* dc, Shader* shader);
+
+	// レイキャスト
+	bool RayCast(const DirectX::XMFLOAT3& start, const DirectX::XMFLOAT3& end, HitResult& hit) override;
+
+	// デバッグGUI
+	void DrawDebugGUI() override;
+
+private:
+	// 行列更新処理
+	void UpdateTransform();
+
+private:
+	std::unique_ptr<Model> model;
+};
